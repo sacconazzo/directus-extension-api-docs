cd4e960acf059d36622ec08492a0e4a5dbf11e32
Giona Righini <sapbasis@alteanet.it>
2022-11-12 11:58:26 +0100

    validator middleware

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import { defineEndpoint } from '@directus/extensions-sdk';
-
+import { Router, Request, Response } from 'express';
 const swaggerUi = require('swagger-ui-express');
+const OpenApiValidator = require('express-openapi-validator');
 const { findWorkspaceDir } = require('@pnpm/find-workspace-dir');
 const directusDir = process.cwd();
 
 const fs = require('fs');
 const path = require('path');
 
-interface config {
+interface oasconfig {
     docsPath?: string;
     tags?: Array<object>;
     paths?: {
@@ -15,7 +16,15 @@ interface config {
     };
 }
 
-function getConfig(): config {
+interface oas {
+    openapi: string;
+    info: object;
+    paths: {
+        [key: string]: any;
+    };
+}
+
+function getConfig(): oasconfig {
     try {
         const configFile = path.join(directusDir, './extensions/endpoints/oasconfig.json');
         return JSON.parse(fs.readFileSync(configFile, 'utf-8'));
@@ -26,10 +35,31 @@ function getConfig(): config {
 
 const config = getConfig();
 
+function validate(router: Router, paths: Array<string>) {
+    if (config?.paths) {
+        const oas: oas = { openapi: '3.0.1', info: { title: '', description: '', version: '' }, paths: {} };
+        for (const path of paths) {
+            oas.paths[path] = config.paths[path];
+        }
+        router.use(
+            OpenApiValidator.middleware({
+                apiSpec: oas,
+            }),
+        );
+        router.use((err: any, _req: Request, res: Response, _next: any) => {
+            res.status(err.status || 500).json({
+                message: err.message,
+                errors: err.errors,
+            });
+        });
+    }
+}
+
 const id = config?.docsPath || 'api-docs';
 
 export default {
     id,
+    validate,
     handler: defineEndpoint((router, { services, exceptions, logger }) => {
         const { ServiceUnavailableException } = exceptions;
         const { SpecificationService } = services;
